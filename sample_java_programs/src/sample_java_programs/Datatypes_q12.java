1577ac742510b89883d8b1095e4266a9d73576a9
dhanyasri-30338 <2420030338@klh.edu.in>
2025-08-20 15:12:07 +0530

    Add files via upload

---
+++ sample_java_programs/src/sample_java_programs/Datatypes_q12.java
@@ -0,0 +1,11 @@
+package sample_java_programs;
+
+public class Datatypes_q12 {
+    public static void main(String[] args) {
+        final int number = 100;
+
+        System.out.println("Initial value: " + number);
+
+        System.out.println("Final value (unchanged): " + number);
+    }
+}
